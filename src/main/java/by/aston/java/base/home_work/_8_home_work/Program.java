487ee76199e58bcbb9e9fd60fb73f9a02ea48786
Marina Lagoikina <marinalagoikina@gmail.com>
2024-05-19 22:25:49 +0300

    update /_8_home_work

--- src/main/java/by/aston/java/base/home_work/_8_home_work/Program.java
+++ src/main/java/by/aston/java/base/home_work/_8_home_work/Program.java
@@ -1,2 +1,7 @@
-package by.aston.java.base.home_work._8_home_work;public class Program {
+package by.aston.java.base.home_work._8_home_work;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
 }
