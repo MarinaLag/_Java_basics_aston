2bcce24cdc4193abc6e3945355ca20f310b057c6
Marina Lagoikina <marinalagoikina@gmail.com>
2024-05-19 22:01:29 +0300

    update .gitignore

---
+++ src/main/java/by/aston/java/base/home_work/_8_home_work/Main.java
@@ -0,0 +1,8 @@
+package by.aston.java.base.home_work._8_home_work;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("123456");
+    }
+}
